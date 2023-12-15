1009844e79e7d309a2c53ba93c01afbd6917b2b7
LucasgAvila <lucasg.avilas@gmail.com>
2023-12-15 18:12:20 -0300

    agregado de estilos a componentes

--- proyecto-angular/src/app/app.module.ts
+++ proyecto-angular/src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { routing, appRoutingProviders } from './app.routing';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutmeComponent } from './component/aboutme/aboutme.component';
+import { ProjectsComponent } from './component/projects/projects.component';
+import { CreateComponent } from './component/create/create.component';
+import { ContactComponent } from './component/contact/contact.component';
+import { ErrorComponent } from './component/error/error.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutmeComponent,
+    ProjectsComponent,
+    CreateComponent,
+    ContactComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    routing
   ],
-  providers: [],
+  providers: [
+    appRoutingProviders
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
