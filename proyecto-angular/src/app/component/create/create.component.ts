7bbacc92aee07001037d48cfc2b4260107a383b1
LucasgAvila <lucasg.avilas@gmail.com>
2023-12-21 10:06:02 -0300

    arreglo en subir proyecto a la DB

--- proyecto-angular/src/app/component/create/create.component.ts
+++ proyecto-angular/src/app/component/create/create.component.ts
@@ -3,7 +3,7 @@ import { Project } from '../../models/project';
 import { ProjectService } from '../../services/project.service';
 import { Observable } from 'rxjs';
 import { UploadService } from '../../services/upload.service';
-
+import { Global } from '../../services/global';
 
 
 @Component({
@@ -16,6 +16,7 @@ export class CreateComponent {
   public title: string;
   public project: Project;
   public status: string;
+  public filesToUpload: Array<File>
 
   constructor(
     private _projectService : ProjectService,
@@ -24,16 +25,24 @@ export class CreateComponent {
     this.title = "Crear proyecto"
     this.project = new Project('', '', '', '', 2023, '', '')
     this.status = "";
+    this.filesToUpload = []
 
 
   }
 
   onSubmit(form:any){
+    //Guardar los datos
     this._projectService.saveProject(this.project).subscribe({
       next: response => {
         if(response.project){
-          this.status = "success"
-          form.reset()
+
+          //Subir la imagen
+          this._uploadService.makeFileRequest(Global.url+'uploadImage/'+response.project._id, [], this.filesToUpload ,'image').then((result:any) => {
+            console.log(result)
+            this.status = "success"
+            form.reset()
+          })
+
         } else {
           this.status = "failed"
         }
@@ -43,4 +52,8 @@ export class CreateComponent {
       }}
     )
   }
+  fileChangeEvent(filenput:any){
+    this.filesToUpload = <Array<File>>filenput.target.files;
+  }
+
 }
